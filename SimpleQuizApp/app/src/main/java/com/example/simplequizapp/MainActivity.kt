45e56365b769dbd2387b9004a20bcdd8bb6de41f
Bhawna chauhan <bhawnabc2002@gmail.com>
2021-12-12 14:51:58 +0530

    done with some more project

--- SimpleQuizApp/app/src/main/java/com/example/simplequizapp/MainActivity.kt
+++ SimpleQuizApp/app/src/main/java/com/example/simplequizapp/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         //before starting mene ye viewbinding kabhi kiya nahi hei
+        //emulator hang ho rha?
+        //u there
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
---
+++ practiceTutorial/app/src/main/java/com/example/practicetutorial/Activity2.kt
@@ -0,0 +1,15 @@
+package com.example.practicetutorial
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class Activity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_2)
+
+
+    }
+}
