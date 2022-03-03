24b30772445eabe161271246763cde2ea80b3582
Bhawna chauhan <bhawnabc2002@gmail.com>
2022-03-03 23:16:20 +0530

    Introduction of Course

---
+++ Udemy Course/First Basics Project/app/src/main/java/com/example/MyApplication/Introduction.kt
@@ -0,0 +1,8 @@
+package com.example.MyApplication
+
+class Introduction {
+    /*
+    Here i am learning about Lecture 1 from denis from this lecture
+    Link : https://www.udemy.com/course/android-kotlin-developer/learn/lecture/16824280#content
+     */
+}
