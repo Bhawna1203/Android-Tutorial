b6569be3387ead076bd4f8e0ce7274a41a960a5c
Bhawna chauhan <bhawnabc2002@gmail.com>
2022-03-11 18:20:55 +0530

    Start Section-4

---
+++ Udemy Course/Oops Basics/app/src/main/java/com/example/oopsbasics/OopsBasicd.kt
@@ -0,0 +1,38 @@
+package com.example.oopsbasics
+
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener
+
+/*
+fun main(){
+    //Here denis is the variable of datatype Person
+    var denis = Person("Denis","Sharma")
+    var john = Person()
+    var johnPeterson = Person(lastName = "Chauhan")
+    var bhawna = Person(firstName = "Bhawna")
+
+}
+class Person constructor(firstName: String = "John", lastName: String = "Doe"){
+
+    //initializer block
+    init{
+        println("Initialised a new Person object with " +
+                "firstName = $firstName and lastname = $lastName")
+
+    }
+
+}
+
+ */
+/*
+//Exercise question
+fun main(){
+    val phone = MobilePhone()
+}
+class MobilePhone(osName:String="Android",brand:String="Samsung",model:String="S20 Ultra"){
+    init{
+        println("$brand Galaxy $model ")
+    }
+}
+
+ */
+//Scope and Shadowing
