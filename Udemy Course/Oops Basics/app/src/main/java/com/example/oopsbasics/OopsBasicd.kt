3722b6b57b422f408b9512bf920a405d4cec26d3
Bhawna chauhan <bhawnabc2002@gmail.com>
2022-03-13 17:55:51 +0530

    Done with Session-4

--- Udemy Course/Oops Basics/app/src/main/java/com/example/oopsbasics/OopsBasicd.kt
+++ Udemy Course/Oops Basics/app/src/main/java/com/example/oopsbasics/OopsBasicd.kt
@@ -1,7 +1,10 @@
 package com.example.oopsbasics
 
+import android.os.Parcel
+import android.os.Parcelable
 import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener
 import java.lang.IllegalArgumentException
+import java.lang.Math.floor
 
 /*
 fun main(){
@@ -218,7 +221,7 @@ class ElectricCar : Car {
  * You can edit, run, and share this code.
  * play.kotlinlang.org
  */
-
+/*
 open class Car(val name: String, val brand: String) {
     // open so it can be overriden by inhereting classes
     open var range: Double = 0.0
@@ -254,7 +257,234 @@ fun main(args: Array<String>){
     // Or alternativly works if Car was a Interface and Electriccar would inherit from it
     teslaS.drive(200.0)
 }
+*/
+/*
+interface Driveable {
+    val maxSpeed: Double
+    fun drive(): String
+    fun brake(){
+        println("The drivable is braking")
+    }
+}
+
+// Class Car which extends the interface
+open class Car(override val maxSpeed: Double,
+               open val brandName: String
+) : Driveable {
+    // open so it can be overriden by inhereting classes
+    open var range: Double = 0.0
+
+    open fun extendRange(amount: Double) {
+        if (amount > 0) {
+            range += amount
+        }
+
+    }
+
+    override fun drive(): String {
+        println("Drove for $range KM")
+        return range.toString()
+    }
+
+    // overridden functions are implicitly open:
+    override fun brake() {
+        println("The car is breaking")
+    }
+}
+// In case there is no primary Constructor
+class ElectricCar(override val maxSpeed: Double,
+                  override val brandName: String,
+                  batteryLife: Double) : Car(maxSpeed, brandName), Parcelable {
+
+    // in KM
+    override var range = batteryLife/6
+
+    constructor(parcel: Parcel) : this(
+        parcel.readDouble(),
+        parcel.readString(),
+        TODO("batteryLife")
+    ) {
+        range = parcel.readDouble()
+    }
+
+    override fun drive() = "Overriding the drive of my Car"
+
+    fun drive(distance: Double){
+        println("Drove for $distance KM on electricity")
+    }
+    override fun brake(){
+        println("The electirc car is breaking")
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeDouble(maxSpeed)
+        parcel.writeString(brandName)
+        parcel.writeDouble(range)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<ElectricCar> {
+        override fun createFromParcel(parcel: Parcel): ElectricCar {
+            return ElectricCar(parcel)
+        }
+
+        override fun newArray(size: Int): Array<ElectricCar?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
 
 
+
+fun main(args: Array<String>){
+    var audiA3 = Car(200.0, "Audi")
+    var teslaS = ElectricCar(250.0, "Tesla", 85.0)
+
+    // Polymorphism is the ability to treat objects
+    // with similar traits in a common way
+    audiA3.drive()
+    // Only works because ElectricCar is a Subclass of Car
+    // Or alternatively works if Car was an Interface and ElectricCar would inherit from it
+    teslaS.drive()
+    teslaS.drive(200.0)
+}
+// Subclasses of a class that implements an interface
+// (in this case, ElectricCar) are also considered
+// to be implementing the interface.
+
+
+*/
+/*
+// An abstract class cannot be instantiated
+// (you cannot create objects of an abstract class).
+// However, you can inherit subclasses from can them.
+// The members (properties and methods) of an abstract class are non-abstract
+// unless you explicitly use the abstract keyword to make them abstract.
+abstract class Mammal(val name: String, val origin: String,
+                      val weight: Double) {   // Concrete (Non Abstract) Properties
+
+    // Abstract Property (Must be overridden by Subclasses)
+    abstract var maxSpeed: Double
+
+    // Abstract Methods (Must be implemented by Subclasses)
+    abstract fun run()
+    abstract fun breath()
+
+    // Concrete (Non Abstract) Method
+    fun displayDetails() {
+        println("Name: $name, Origin: $origin, Weight: $weight, " +
+                "Max Speed: $maxSpeed")
+    }
+}
+
+class Human(name: String, origin: String, weight: Double,
+            override var maxSpeed: Double): Mammal(name, origin, weight) {
+
+    override fun run() {
+        // Code to run
+        println("Runs on two legs")
+    }
+
+    override fun breath() {
+        // Code to breath
+        println("Breath through mouth or nose")
+    }
+}
+
+class Elephant(name: String, origin: String, weight: Double,
+               override var maxSpeed: Double): Mammal(name, origin, weight) {
+
+    override fun run() {
+        // Code to run
+        println("Runs on four legs")
+    }
+
+    override fun breath() {
+        // Code to breath
+        println("Breath through the trunk")
+    }
+}
+
+fun main() {
+    val human = Human("Denis", "Russia",
+        70.0, 28.0)
+    val elephant = Elephant("Rosy", "India",
+        5400.0, 25.0)
+
+    human.run()
+    elephant.run()
+
+    human.breath()
+    elephant.breath()
+
+}
+
+ */
+fun main(){
+    val stringList: List<String> = listOf(
+        "Denis","Frank","Michael","Garry"
+    )
+    val mixedTypeList: List<Any> = listOf(
+        "Denis",31,23,"Frankly",23.45,"kg"
+    )
+
+    for(value in mixedTypeList){
+        if (value is Int) {
+            println("Integer : '$value'")
+        }
+        else if (value is Double) {
+            println("Double: '$value' with Floor value ${floor(value)}")
+        }
+        else if (value is String) {
+            println("String: '$value' of length ${value.length}")
+        }
+        else {
+            println("Unknown type")
+        }
+    }
+    //Alternatively
+    for(value in mixedTypeList){
+        when (value) {
+            is Int -> {
+                println("Integer : '$value'")
+            }
+            is Double -> {
+                println("Double: '$value' with Floor value ${floor(value)}")
+            }
+            is String -> {
+                println("String: '$value' of length ${value.length}")
+            }
+            else -> {
+                println("Unknown type")
+            }
+        }
+    }
+
+    //SMART CAST
+    val obj1:Any = "I have a dream"
+    if(obj1 !is String){
+        println("Not a String")
+    }else{
+        //obj is automatically cast to a string in this scope
+        println("Found a String of Length ${obj1.length}")
+    }
+
+
+    //Explicit (unsafe) Casting using the "as" keyword - can go wrong
+    val str1:String = obj1 as String
+    println(str1.length)
+
+    val obj2: Any = 1337
+    val str2:String = obj2 as String
+    println(str2)
+
+    //Explicit (safe) casting using the "as?" keyword
+    val obj3: Any = 1337
+    val str3:String ?= obj3 as? String //Works
+
+}
 
 
