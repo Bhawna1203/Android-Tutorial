bfeeaeb07ac123e4133acd9df7872efc47302fd9
Bhawna chauhan <bhawnabc2002@gmail.com>
2022-04-15 18:19:06 +0530

    working on another app i.e HappyPlaceApp

--- Udemy Course/7MinutesWorkout/app/src/main/java/com/example/a7minutesworkout/ExerciseActivity.kt
+++ Udemy Course/7MinutesWorkout/app/src/main/java/com/example/a7minutesworkout/ExerciseActivity.kt
@@ -1,14 +1,21 @@
 package com.example.a7minutesworkout
 
+import android.media.MediaPlayer
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.provider.MediaStore
+import android.speech.tts.TextToSpeech
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import com.example.a7minutesworkout.databinding.ActivityExerciseBinding
 import com.example.a7minutesworkout.databinding.ActivityMainBinding
+import java.util.*
+import kotlin.collections.ArrayList
 
-class ExerciseActivity : AppCompatActivity() {
+class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
 
     private var binding: ActivityExerciseBinding? = null
 
@@ -20,6 +27,9 @@ class ExerciseActivity : AppCompatActivity() {
 
     private var exerciseList : ArrayList<ExerciseModel>? = null
     private var currentExercisePosition = -1
+
+    private var tts: TextToSpeech? = null
+    private var player:MediaPlayer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,6 +43,8 @@ class ExerciseActivity : AppCompatActivity() {
 
         exerciseList = Constants.defaultExerciseList()
 
+        tts = TextToSpeech(this, this)
+
         binding?.toolbarExercise?.setNavigationOnClickListener{
             onBackPressed()
         }
@@ -44,6 +56,18 @@ class ExerciseActivity : AppCompatActivity() {
      * Function is used to set the timer for REST.
      */
     private fun setupRestView() {
+
+        try{
+            val soundURI = Uri.parse(
+                "android.resource://com.example.a7minutesworkout/" +
+                        R.raw.app_src_main_res_raw_press_start
+            )
+            player = MediaPlayer.create(applicationContext, soundURI)
+            player?.isLooping = false
+            player?.start()
+        }catch (e: Exception){
+            e.printStackTrace()
+        }
 
         binding?.flRestView?.visibility = View.VISIBLE
         binding?.tvTitle?.visibility = View.VISIBLE
@@ -111,6 +135,8 @@ class ExerciseActivity : AppCompatActivity() {
             exerciseProgress = 0
         }
 
+        speakOut(exerciseList!![currentExercisePosition].getName())
+
         binding?.ivImage?.setImageResource(exerciseList!![currentExercisePosition].getImage())
         binding?.tvExerciseName?.text = exerciseList!![currentExercisePosition].getName()
 
@@ -153,7 +179,46 @@ class ExerciseActivity : AppCompatActivity() {
             restProgress = 0
         }
         super.onDestroy()
+
+        if(exerciseTimer != null){
+            exerciseTimer?.cancel()
+            exerciseProgress=0
+        }
+
+        if (tts != null) {
+            tts?.stop()
+            tts?.shutdown()
+        }
+
+        if(player != null){
+            player?.stop()
+
+        }
+
         binding = null
+
+    }
+
+    override fun onInit(status: Int) {
+        if (status == TextToSpeech.SUCCESS) {
+            // set US English as language for tts
+            val result = tts!!.setLanguage(Locale.US)
+
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("TTS", "The Language specified is not supported!")
+            }
+
+        } else {
+            Log.e("TTS", "Initialization Failed!")
+        }
+    }
+
+
+    /**
+     * Function is used to speak the text what we pass to it.
+     */
+    private fun speakOut(text: String) {
+        tts?.speak(text, TextToSpeech.QUEUE_ADD, null, "")
     }
 
 
