0e659bb25dec879755ba723c0b16fab46ce29cd1
Bhawna chauhan <bhawnabc2002@gmail.com>
2022-04-16 23:09:25 +0530

    Done with the concept of ViewBinding

---
+++ Udemy Course/ViewBindingRv/app/src/main/java/com/example/viewbindingrv/TaskList.kt
@@ -0,0 +1,50 @@
+package com.example.viewbindingrv
+
+object TaskList {
+    val taskList = listOf<Task>(
+        Task(
+            "Take a rest",
+            "7:00 am"
+        ),
+        Task(
+            "Take a rest",
+            "7:00 am"
+        ),
+        Task(
+            "Take a rest",
+            "7:00 am"
+        ),
+        Task(
+            "Take a rest",
+            "7:00 am"
+        ),
+        Task(
+            "Take a rest",
+            "7:00 am"
+        ),
+        Task(
+            "Take a rest",
+            "7:00 am"
+        ),
+        Task(
+            "Take a rest",
+            "7:00 am"
+        ),
+        Task(
+            "Take a rest",
+            "7:00 am"
+        ),
+        Task(
+            "Take a rest",
+            "7:00 am"
+        ),
+        Task(
+            "Take a rest",
+            "7:00 am"
+        ),
+        Task(
+            "Take a rest",
+            "7:00 am"
+        )
+    )
+}
