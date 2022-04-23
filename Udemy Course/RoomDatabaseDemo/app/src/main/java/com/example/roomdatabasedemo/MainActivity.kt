3ba3dd9ef059848db0a8e8e6f49f5f0747ce4e42
Bhawna chauhan <bhawnabc2002@gmail.com>
2022-04-23 12:33:54 +0530

    Done with adding database in App inspection

--- Udemy Course/RoomDatabaseDemo/app/src/main/java/com/example/roomdatabasedemo/MainActivity.kt
+++ Udemy Course/RoomDatabaseDemo/app/src/main/java/com/example/roomdatabasedemo/MainActivity.kt
@@ -1,11 +1,52 @@
 package com.example.roomdatabasedemo
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
+import com.example.roomdatabasedemo.databinding.ActivityMainBinding
+import com.example.roomdatabasedemo.databinding.DialogUpdateBinding
+import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMainBinding? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        val employeeDao = (application as EmployeeApp).db.employeeDao()
+        binding?.btnAdd?.setOnClickListener{
+           addRecord(employeeDao)
+        }
+
+
+    }
+
+    fun addRecord(employeeDao: EmployeeDao){
+        val name = binding?.etName?.text.toString()
+        val email = binding?.etEmailId?.text.toString()
+
+        if(name.isNotEmpty() && email.isNotEmpty()){
+            lifecycleScope.launch {
+                employeeDao.insert(EmployeeEntity(name = name, email = email))
+                Toast.makeText(applicationContext,"Record saved",Toast.LENGTH_LONG).show()
+                binding?.etName?.text?.clear()
+                binding?.etEmailId?.text?.clear()
+            }
+        }else{
+            Toast.makeText(
+                applicationContext,
+                "Name or email cannt be Blank",
+                Toast.LENGTH_LONG
+            ).show()
+        }
     }
 }
+
+
